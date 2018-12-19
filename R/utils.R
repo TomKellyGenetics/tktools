bf6bb11181ef36cc6a32947c17cb9d0e5b7602ef
TomKellyGenetics <tomkellygenetics@gmail.com>
2018-12-19 22:54:32 +0900

    add string splitter

--- R/utils.R
+++ R/utils.R
@@ -1,18 +1,27 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-splitstrings <- function() {
-  lapply(strplit)
+#' plot
+#'
+#' plot with legend and title, mtext added
+#'
+#' @param strings vector or list of strings to split
+#' @param n indicies of each string to extract (and paste back together)
+#' @param split character to split by
+#' @keywords plot graphics annotation title legend
+#' @importFrom graphics plot
+#' @export
+#' @examples
+#'
+#' # example string
+#' string <- c("1-4-5", "4-6-3", "1-0-3")
+#' splitstrings(string, 2)
+#'
+#' @export
+#' @usage NULL
+splitstrings <- function(strings, n = 1, split="-") {
+  if(length(n) == 1){
+    extract <- function(string) strsplit(string, split = split)[[1]][n]
+  } else {
+    extract <- function(string) paste(strsplit(string, split = split)[[1]][n], collapse = split)
+  }
+  sapply(strings, extract)
 }
+
