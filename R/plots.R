6e9e9e03e3c35e77d30564c9a43934f5bad5757a
TomKellyGenetics <tomkellygenetics@gmail.com>
2019-11-27 15:06:51 +0900

    add colour palette for ggplot colours

--- R/plots.R
+++ R/plots.R
@@ -138,3 +138,38 @@ plot <- function(..., legend = NULL, title = NULL, mtext = NULL, facet = NULL){
   #  graphics::plot(...)
   #}
 }
+
+#' gg_color_hue
+#'
+#' emulate colour scale for ggplots
+#'
+#' @param n the number of colors (≥ 1) to be in the palette.
+#' @keywords plot graphics annotation title legend
+#' @importFrom graphics plot
+#' @export
+#' @examples
+#'
+#' # example plot
+#' n <- 5
+#' x_mean <- rnorm(n, 1000, 500)
+#' y_mean <- rnorm(n, 1000, 500)
+#' x_vals <- sapply(x_mean, function(x) rnorm(100, x, 50))
+#' y_vals <- sapply(y_mean, function(y) rnorm(100, y, 50))
+#' plot(x_vals, y_vals, col = colourscale(n, brewer.pal(11, "Set3"))[unlist(lapply(1:n, function(x) rep(x, 100)))], pch = 20,
+#'      legend = legend("topleft", title = "colours", legend = 1:n, col = colourscale(n, brewer.pal(11, "Set3")), pch = 20),
+#'      main = "title", xlab = "x-axis", ylab = "y-axis")
+#'
+#' @export
+#' @usage NULL
+
+
+gg_color_hue <- function(n){
+  if(round(n) == n){
+    n <- as.integer(n)
+  } else {
+    stop("n must be an integer")
+  }
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+
